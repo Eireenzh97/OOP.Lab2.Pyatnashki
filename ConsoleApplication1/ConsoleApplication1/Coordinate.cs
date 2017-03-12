4080a2b491cfc4bd901ee927ccea5a3a14c12a70
Ирина Жиляева <eireen.zhilyaeva.97@gmail.com>
2017-03-12 11:54:06 +0500

    класс Координаты

---
+++ ConsoleApplication1/ConsoleApplication1/Coordinate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Coordinate
+    {
+        int x;
+        int y;
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+        public Coordinate(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+    }
+}
+
